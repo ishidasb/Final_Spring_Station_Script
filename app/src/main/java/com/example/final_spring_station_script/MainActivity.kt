93e1955491bf285382affe0f96248156f6e822da
ishidasb <ishidasb@mail.uc.edu>
2022-04-19 16:38:01 -0400

    firebase and save user

--- app/src/main/java/com/example/final_spring_station_script/MainActivity.kt
+++ app/src/main/java/com/example/final_spring_station_script/MainActivity.kt
@@ -15,85 +15,100 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-
 import androidx.compose.foundation.layout.fillMaxWidth
+import com.example.final_spring_station_script.dto.User
 import com.example.final_spring_station_script.ui.theme.Final_Spring_Station_ScriptTheme
-//import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModel<MainViewModel>()
+    private var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Final_Spring_Station_ScriptTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    color = MaterialTheme.colors.background,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    CarPartFacts("Android")
+            viewModel.fetchComputerComponents()
+            firebaseUser?.let {
+                val user = User(it.uid, "")
+                viewModel.user = user
+                viewModel.listenToParts()
+                Final_Spring_Station_ScriptTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        color = MaterialTheme.colors.background,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        CarPartFacts("Android")
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun CarPartFacts(name: String) {
-    var computerPartName by remember { mutableStateOf(" ") }
-    var computerPartType by remember { mutableStateOf("") }
-    var computerPartBrand by remember { mutableStateOf("") }
-    var computerPartRating by remember { mutableStateOf("") }
-    var computerPartPrice by remember { mutableStateOf("") }
-    val context = LocalContext.current
-    Column() {
+    @Composable
+    fun CarPartFacts(name: String) {
+        var computerPartName by remember { mutableStateOf(" ") }
+        var computerPartType by remember { mutableStateOf("") }
+        var computerPartBrand by remember { mutableStateOf("") }
+        var computerPartRating by remember { mutableStateOf("") }
+        var computerPartPrice by remember { mutableStateOf("") }
+        val context = LocalContext.current
+        Column() {
 
-        OutlinedTextField(
-            value = computerPartName,
-            onValueChange = { computerPartName = it },
-            label =  {Text(stringResource(R.string.partName))},
-            modifier = Modifier.fillMaxWidth()
-        )
+            OutlinedTextField(
+                value = computerPartName,
+                onValueChange = { computerPartName = it },
+                label = { Text(stringResource(R.string.partName)) },
+                modifier = Modifier.fillMaxWidth()
+            )
 
-        OutlinedTextField(
-            value = computerPartType,
-            onValueChange = { computerPartType = it },
-            label =  {Text(stringResource(R.string.partType))},
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = computerPartBrand,
-            onValueChange = { computerPartBrand = it },
-            label =  {Text(stringResource(R.string.partBrand))},
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = computerPartRating,
-            onValueChange = { computerPartRating = it },
-            label = { Text(stringResource(R.string.partRating))},
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = computerPartPrice,
-            onValueChange = { computerPartPrice = it },
-            label =  {Text(stringResource(R.string.partPrice))},
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button (
-            onClick = {
-                Toast.makeText(context, "$computerPartName $computerPartType $computerPartBrand $computerPartRating $computerPartPrice", Toast.LENGTH_LONG).show()
-            },
-            content = { Text(text = "Save")}
-        )
+            OutlinedTextField(
+                value = computerPartType,
+                onValueChange = { computerPartType = it },
+                label = { Text(stringResource(R.string.partType)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = computerPartBrand,
+                onValueChange = { computerPartBrand = it },
+                label = { Text(stringResource(R.string.partBrand)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = computerPartRating,
+                onValueChange = { computerPartRating = it },
+                label = { Text(stringResource(R.string.partRating)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = computerPartPrice,
+                onValueChange = { computerPartPrice = it },
+                label = { Text(stringResource(R.string.partPrice)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(
+                onClick = {
+                    Toast.makeText(
+                        context,
+                        "$computerPartName $computerPartType $computerPartBrand $computerPartRating $computerPartPrice",
+                        Toast.LENGTH_LONG
+                    ).show()
+                },
+                content = { Text(text = "Save") }
+            )
 
 
-
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Final_Spring_Station_ScriptTheme {
-        CarPartFacts("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        Final_Spring_Station_ScriptTheme {
+            CarPartFacts("Android")
+        }
     }
 }
--- app/src/main/java/com/example/final_spring_station_script/MainViewModel.kt
+++ app/src/main/java/com/example/final_spring_station_script/MainViewModel.kt
@@ -1,26 +1,85 @@
 package com.example.final_spring_station_script
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.final_spring_station_script.dto.ComputerComponent
 import com.example.final_spring_station_script.dto.User
-//import com.google.firebase.firestore.FirebaseFirestore
-//import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.example.final_spring_station_script.service.ComponentService
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import kotlinx.coroutines.launch
 
 
 class MainViewModel (get: Any) : ViewModel(){
-    /*var user: User? = null
+
+    var computerComponent by mutableStateOf(ComputerComponent())
+    var componentService: ComponentService = ComponentService()
+    val NEW_Computer = "New Component"
+    var components: MutableLiveData<List<ComputerComponent>> =
+        MutableLiveData<List<ComputerComponent>>()
+    var user: User? = null
     private lateinit var firestore: FirebaseFirestore
+
 
     init {
         firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
     }
-
+    fun fetchComputerComponents() {
+        viewModelScope.launch {
+            var innerComponents = componentService.fetchComputerComponents()
+            components.postValue(innerComponents)
+        }
+    }
+    fun saveParts() {
+        user?.let { user ->
+            val document =
+                if (computerComponent.Type == null || computerComponent.Type.isEmpty()) {
+                    // insert
+                    firestore.collection("users").document(user.uid).collection("Parts")
+                        .document()
+                } else {
+                    // update
+                    firestore.collection("users").document(user.uid).collection("Parts")
+                        .document(computerComponent.Type)
+                }
+        }
+    }
     fun saveUser() {user?.let { user ->
         val handle = firestore.collection("users").document(user.uid).set(user)
         handle.addOnSuccessListener { Log.d("Firebase", "User Saved") }
         handle.addOnFailureListener { Log.e("Firebase", "User save failed $it") }
-
         }
-    }*/
+    }
+
+    fun listenToParts() {
+        user?.let { user ->
+            firestore.collection("users").document(user.uid).collection("")
+                .addSnapshotListener { snapshot, error ->
+                    // see of we received an error
+                    if (error != null) {
+                        Log.w("listen failed.", error)
+                        return@addSnapshotListener
+                    }
+                    // if we reached this point, there was not an error, and we have data.
+                    snapshot?.let {
+                        val allParts = ArrayList<ComputerComponent>()
+                        allParts.add(ComputerComponent(NEW_Computer))
+                        val documents = snapshot.documents
+                        documents.forEach {
+                            val computer = it.toObject(ComputerComponent::class.java)
+                            computer?.let {
+                                allParts.add(computer)
+                            }
+                        }
+                        ComputerComponent.value = allParts
+                    }
+                }
+        }
+    }
 }
