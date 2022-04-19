916d86b3ccbdc81585a0a59887487c711c499eaf
ishidasb <ishidasb@mail.uc.edu>
2022-04-19 11:03:33 -0400

    Initial commit

---
+++ app/src/main/java/com/example/final_spring_station_script/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.final_spring_station_script
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.final_spring_station_script.ui.theme.Final_Spring_Station_ScriptTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Final_Spring_Station_ScriptTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Final_Spring_Station_ScriptTheme {
+        Greeting("Android")
+    }
+}
