420fb3f168b85311e85392d3e19c803c37b29a95
ishidasb <ishidasb@mail.uc.edu>
2022-04-19 12:08:16 -0400

    fixed_Main_View_model,app_module and build gradle

--- app/src/main/java/com/example/final_spring_station_script/MainViewModel.kt
+++ app/src/main/java/com/example/final_spring_station_script/MainViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.final_spring_station_script
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.final_spring_station_script.dto.User
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MainViewModel (get: Any) : ViewModel(){
+    var user: User? = null
+    private lateinit var firestore: FirebaseFirestore
+    fun saveUser() {user?.let { user ->
+        val handle = firestore.collection("users").document(user.uid).set(user)
+        handle.addOnSuccessListener { Log.d("Firebase", "User Saved") }
+        handle.addOnFailureListener { Log.e("Firebase", "User save failed $it") }
+
+    }
+    }
 }
--- app/src/main/java/com/example/final_spring_station_script/service/appModule.kt
+++ app/src/main/java/com/example/final_spring_station_script/service/appModule.kt
@@ -1,10 +1,10 @@
 package com.example.springprojectstationbuild.service
 
-import com.example.springprojectstationbuild.MainViewModel
+import com.example.final_spring_station_script.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-/*val appModule = module {
+val appModule = module {
     viewModel { MainViewModel(get()) }
-    single<IComponentService> { ComponentService() }
-}*/
+    //single<IComponentService> { ComponentService() }
+}
