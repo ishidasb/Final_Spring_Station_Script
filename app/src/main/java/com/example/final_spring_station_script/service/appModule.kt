75b3363b8c1bcc887f222247b913b25de223e324
ishidasb <ishidasb@mail.uc.edu>
2022-04-19 11:49:48 -0400

    add folders and build gradle file

---
+++ app/src/main/java/com/example/final_spring_station_script/service/appModule.kt
@@ -0,0 +1,10 @@
+package com.example.springprojectstationbuild.service
+
+import com.example.springprojectstationbuild.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+/*val appModule = module {
+    viewModel { MainViewModel(get()) }
+    single<IComponentService> { ComponentService() }
+}*/
