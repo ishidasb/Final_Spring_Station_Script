6948d9826de52c1e6fde994ea55cf7fdf4ff8d04
James <48566053+Jovial-Developer@users.noreply.github.com>
2022-04-19 22:36:04 -0400

    This new dataClass is for working with Firebase. The first DTO is for launch

---
+++ app/src/main/java/com/example/final_spring_station_script/dto/SpecifiedComputerPart.kt
@@ -0,0 +1,14 @@
+package com.example.final_spring_station_script.dto
+
+data class SpecifiedComputerPart(
+
+    var thisPartType : String = "",
+    var thisPartName :String = "",
+    var thisPartBrand : String = "",
+    var thisPartPrice : Double = 0.0,
+    var thisPartRating : Int = 0
+    ) {
+    override fun toString(): String {
+        return "$thisPartType, $thisPartName, $thisPartBrand, $thisPartPrice, $thisPartRating"
+    }
+}
